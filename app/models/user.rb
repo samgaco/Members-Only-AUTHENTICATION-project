f9882ebd6725f134e6f65fa54aefc31df0a91617
Samuel <samuelgarciacompanys@gmail.com>
2019-07-30 17:50:02 +0200

    worked on sign-in until 5th step

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
+    before_create :create_activation_digest
+
+
 
     validates :name , presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,5 +11,24 @@ class User < ApplicationRecord
     has_secure_password
     # validates :password , presence: true
     # validates :password_confirmation , presence: true
+
+    def remember 
+        self.remember_token = User.token.def new
+        update_attribute(:remember_digest , User.digest(remember_token)) 
+    end 
+
+
+    def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
+
+    def self.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)     
+    end 
     
+    def self.new_token
+        SecureRandom.urlsafe_base64
+    end 
 end
